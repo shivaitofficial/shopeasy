c36a3a4eee8410e1c42407f726d503dbf8db5292
shivaitofficial <shivaitofficial@gmail.com>
2026-04-28 11:17:26 +0530

    App.js: New const variable added

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const name="IT";
 app.get("/",(req,res) => {
-    res.send("Welcome to ShopEasy Shiva Done Official !!!!");
+    res.send(`Welcome to ShopEasy Shiva ${name} Official !!!!`);
 })
 const server = app.listen(PORT,() =>{
     console.log(`Server is Running on port ${PORT}`);
 })
 
-module.exports = {app,server}
+module.exports = {app,server,name}
--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
-const {app,server} = require('../app');
+const {app,server,name} = require('../app');
 
 describe('GET /',()=>{
     it("Should return 200 status and the correct message",async()=>{
         const response = await request(app).get("/");
         expect(response.status).toBe(200);
-        expect(response.text).toBe("Welcome to ShopEasy Shiva IT Official !!!!");
+        expect(response.text).toBe(`Welcome to ShopEasy Shiva ${name} Official !!!!`);
     })
 })
 
